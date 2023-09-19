fa4f7b5ed7b254855ac07755f6e0f6f7748eea1d
Masuma01~ <javariamasooma.bscssef20@iba-suk.edu.pk>
2023-09-19 13:15:58 +0500

    modified

--- script.js
+++ script.js
@@ -4,3 +4,18 @@ function myname(){
 }
 myname();
 
+//=============================Variable Declaration===================================//
+const a = 8;
+const b = true;
+const c = "Javaria Masooma";
+const date = new Date().toLocaleDateString;
+const d=undefined;
+const e = [1,2,3,4,5,6,7,8,9,10];
+const f = ["Saima", "Anam", "Fatima"];
+const g = [2, 4,"Ahmad", 6, true];
+const h = {
+    ID : 1,
+    Name : "Javaria"
+};
+
+
